a64b882184345cc80130714f57fd46cc15e2c188
lsocpb <lukasssonik@gmail.com>
2025-03-08 15:04:34 +0100

    Dodanie zależności i plików do projektu.

--- frontend/todo-list/app/page.tsx
+++ frontend/todo-list/app/page.tsx
@@ -3,9 +3,11 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        TODO List
+    <div className="min-h-screen bg-gray-100">
+      <main className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold text-gray-800">
+          TODO List
+        </h1>
       </main>
     </div>
   );
